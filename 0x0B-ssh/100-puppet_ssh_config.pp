e6f574fdf93f8cb7146b1be2253dacd22ef45ddf
laila22haz <hazmirlaila316@gmail.com>
2023-09-25 01:52:09 +0100

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# configuration file
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  line   => '    PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  line   => '    IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
+}
