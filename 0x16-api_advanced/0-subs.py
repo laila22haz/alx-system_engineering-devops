4853cfe1a01d1e593b3b23fe45df499c273f07d0
laila22haz <hazmirlaila316@gmail.com>
2023-11-07 23:14:23 +0100

    Task : task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """How many subs"""
-import requests
+from urllib import request
 import json
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    request = requests.get(url, headers={'User-agent': 'Chrome'})
-    json_load = json.loads(request.text)
-    if request.status_code == 200:
-        find_subscribers = json_load.get("data").get("subscribers")
-        return find_subscribers
-    else:
+    r = request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    try:
+        response = request.urlopen(r)
+        if response.getcode() == 200:
+            data = response.read().decode('utf-8')
+            json_load = json.loads(data)
+            find_subscribers = json_load.get("data")["subscribers"]
+            return find_subscribers
+        else:
+            return 0
+    except request.HTTPError as e:
         return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""How many subs"""
+import requests
+import json
+
+
+def top_ten(subreddit):
+    """return the Top ten"""
+    
