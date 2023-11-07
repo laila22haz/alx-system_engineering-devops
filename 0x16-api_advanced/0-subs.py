20fdde1d40d7cdee395d76920e37e0490beeb56e
laila22haz <hazmirlaila316@gmail.com>
2023-11-07 23:22:12 +0100

    Task : task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import json
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    request = requests.get(url, headers={'User-agent': 'Reddit Subscribers Count Bot'})
+    request = requests.get(url, headers={'User-agent': 'Chrome'})
     json_load = json.loads(request.text)
     if request.status_code == 200:
         find_subscribers = json_load.get("data")["subscribers"]
