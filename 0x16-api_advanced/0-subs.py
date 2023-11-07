937e97e3c5704fc30f562aa48021dfc4328f8d89
laila22haz <hazmirlaila316@gmail.com>
2023-11-07 23:18:22 +0100

    Task : task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """How many subs"""
-from urllib import request
+import requests
 import json
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    r = request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    try:
-        response = request.urlopen(r)
-        if response.getcode() == 200:
-            data = response.read().decode('utf-8')
-            json_load = json.loads(data)
-            find_subscribers = json_load.get("data")["subscribers"]
-            return find_subscribers
-        else:
-            return 0
-    except request.HTTPError as e:
+    request = requests.get(url, headers={'User-Agent': 'Reddit Subscribers Count Bot'})
+    json_load = json.loads(request.text)
+    if request.status_code == 200:
+        find_subscribers = json_load.get("data")["subscribers"]
+        return find_subscribers
+    else:
         return 0
