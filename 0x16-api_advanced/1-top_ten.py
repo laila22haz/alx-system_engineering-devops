0e83928c70b4ffcdbd173716b9bdcb47c2c6a949
laila22haz <hazmirlaila316@gmail.com>
2023-11-08 01:46:39 +0100

    Task : task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """How many subs"""
 import requests
-import json
 
 
 def top_ten(subreddit):
     """return the Top ten"""
-    
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'Chrome'}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        hot_posts = data.get('data', {}).get('children', [])
+        for post in hot_posts[:10]:
+            title = post['data']['title']
+            print(title)
+    else:
+        print(None)
