57777161ea59b2e7e27ad0141fb3f73785ea52b9
laila22haz <hazmirlaila316@gmail.com>
2023-11-08 01:50:00 +0100

    Task : task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,19 @@
-#!/usr/bin/python3
-"""How many subs"""
 import requests
 
-
 def top_ten(subreddit):
-    """return the Top ten"""
+    """Prints the titles of the top ten hot posts for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': 'Chrome'}
 
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        hot_posts = data.get('data', {}).get('children', [])
-        for post in hot_posts[:10]:
-            title = post['data']['title']
-            print(title)
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            hot_posts = data.get('data', {}).get('children', [])
+            for post in hot_posts[:10]:
+                title = post['data']['title']
+                print(title)
+        else:
+            print(None)
+    except requests.RequestException as e:
         print(None)
