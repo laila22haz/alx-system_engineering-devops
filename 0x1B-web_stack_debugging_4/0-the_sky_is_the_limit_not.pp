c332f20bbe779bb81ce3c4b48645fd9e122a3cac
laila22haz <hazmirlaila316@gmail.com>
2023-11-17 01:15:05 +0100

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,4 @@
 exec { 'adjust_ulimit_and_restart_nginx':
   command     => "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' /etc/default/nginx && /usr/sbin/service nginx restart",
   path        => ['/bin', '/usr/bin', '/usr/sbin'],
-  refreshonly => true,
 }
